b61c9c49defb512689420e585ab1a7687c854fb8
JonathanNiehenke <jrniehenke@gmail.com>
2017-11-06 13:50:43 -0800

    Include tile highlighting

--- towerDefense.js
+++ towerDefense.js
@@ -2,6 +2,7 @@ function GameObj(canvas) {
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
     this.frame = 0;
+    this.mouseGridPos = new PointObj(-1, -1);
     this.sprites = {
         "roads": new SpriteObj(this.context,  "sprites/tileSheet.png", 2, 4),
         "slime": new SpriteObj(this.context, "sprites/SlimeIso.png", 4, 4),
@@ -9,6 +10,28 @@ function GameObj(canvas) {
     this.map = new MapObj(new TileSetObj(this.sprites["roads"]));
     this.isometricSize = this.map.isometricSize;
     this.waves = [];
+    this.mouseToGrid = function(e) {
+        let rect = canvas.getBoundingClientRect();
+        let mousePos = new PointObj(e.clientX - rect.x - this.canvas.width / 2,
+                                    e.clientY - rect.y, "isometric");
+        return this.map.getGridPos(mousePos);
+    };
+    this.mouseMove = function(e) {
+        this.mouseGridPos = this.mouseToGrid(e);
+    };
+    this.highlightTile = function(gPoint) {
+        let iPoint = this.map.gridToIso(gPoint);
+        this.context.strokeStyle = "silver";
+        this.context.fillStyle = "rgba(255, 255, 255, 0.20)";
+        this.context.beginPath();
+        this.context.moveTo(iPoint.x, iPoint.y);
+        this.context.lineTo(iPoint.x + 50, iPoint.y + 25);
+        this.context.lineTo(iPoint.x, iPoint.y + 50);
+        this.context.lineTo(iPoint.x - 50, iPoint.y + 25);
+        this.context.closePath();
+        this.context.stroke();
+        this.context.fill();
+    };
     this.init = function() {
         this.map.applyLevel({
             "mapArray": [
@@ -26,6 +49,7 @@ function GameObj(canvas) {
         let wave = new WaveObj(this.sprites["slime"], 6, this.map.startPoint,
                                this.map.initialHeading);
         this.waves.push(wave);
+        this.canvas.addEventListener("mousemove", this.mouseMove.bind(this));
     };
     this.getNewCreepHeading = function(creep) {
         let gridPos = this.map.getGridPos(creep.point);
@@ -42,6 +66,8 @@ function GameObj(canvas) {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.context.translate(this.canvas.width / 2, 0);
         this.map.draw();
+        if (this.map.isMap(this.mouseGridPos))
+            this.highlightTile(this.mouseGridPos);
         for (wave of this.waves)
             wave.draw();
         this.context.restore();
